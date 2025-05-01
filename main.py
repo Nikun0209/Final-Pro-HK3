1eaaf8e8f4b9b7927ff1e3bbc1b02b799aa458b1
NikunCodeX <72957939+Nikun0209@users.noreply.github.com>
2025-05-01 17:03:16 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def load_model(model_name):
     
     # Kiểm tra xem mô hình có đang ở meta tensor không
     if model.device.type == 'meta':
-        model = model.to_empty(torch.device('cpu'))  # Chuyển sang CPU (hoặc GPU nếu cần)
+        model = model.to_empty(torch.device('gpu'))  # Chuyển sang CPU (hoặc GPU nếu cần)
     
     return model
 
